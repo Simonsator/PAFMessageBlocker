fcf7fe12102482c78dc0b1159924c18a3fd2752a
Simon Brungs <00pflaume@gmail.com>
2017-04-04 22:16:03 +0200

    First upload

---
+++ src/main/java/de/simonsator/partyandfriends/chatinspector/PAFChatInspector.java
@@ -0,0 +1,37 @@
+package de.simonsator.partyandfriends.chatinspector;
+
+import de.simonsator.partyandfriends.api.PAFExtension;
+import de.simonsator.partyandfriends.chatinspector.configuration.ChatInspectorConfig;
+import de.simonsator.partyandfriends.chatinspector.listener.ChatListener;
+import de.simonsator.partyandfriends.main.Main;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.config.Configuration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PAFChatInspector extends PAFExtension {
+
+	@Override
+	public void onEnable() {
+		Main.getInstance();
+		try {
+			Configuration config = (new ChatInspectorConfig(new File(getDataFolder(), "config.yml"))).getCreatedConfiguration();
+			ProxyServer.getInstance().getPluginManager().registerListener(this,
+					new ChatListener(config.getStringList("ForbiddenWords"), config));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onDisable() {
+		ProxyServer.getInstance().getPluginManager().unregisterListeners(this);
+	}
+
+	@Override
+	public void reload() {
+		onDisable();
+		onEnable();
+	}
+}
---
+++ src/main/java/de/simonsator/partyandfriends/chatinspector/listener/ChatListener.java
@@ -0,0 +1,53 @@
+package de.simonsator.partyandfriends.chatinspector.listener;
+
+import de.simonsator.partyandfriends.api.events.message.FriendMessageEvent;
+import de.simonsator.partyandfriends.api.events.message.PartyMessageEvent;
+import de.simonsator.partyandfriends.api.events.message.SimpleMessageEvent;
+import de.simonsator.partyandfriends.api.pafplayers.OnlinePAFPlayer;
+import de.simonsator.partyandfriends.friends.commands.Friends;
+import de.simonsator.partyandfriends.party.command.PartyCommand;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.event.EventHandler;
+
+import java.util.List;
+
+public class ChatListener implements Listener {
+	private final List<String> FORBIDDEN;
+	private final String DO_NOT_WRITE_THAT;
+	private final String PERMISSION;
+
+	public ChatListener(List<String> pList, Configuration pConfig) {
+		for (int i = 0; i < pList.size(); i++)
+			pList.set(i, pList.get(i).toLowerCase());
+		FORBIDDEN = pList;
+		PERMISSION = pConfig.getString("Permission.Ignore");
+		DO_NOT_WRITE_THAT = pConfig.getString("Messages.DoNotWriteThat");
+	}
+
+	@EventHandler
+	public void onChat(PartyMessageEvent pEvent) {
+		if (cancelled(pEvent))
+			pEvent.getSender().sendMessage(PartyCommand.getInstance().getPrefix() + DO_NOT_WRITE_THAT);
+	}
+
+	@EventHandler
+	public void onChat(FriendMessageEvent pEvent) {
+		if (cancelled(pEvent))
+			pEvent.getSender().sendMessage(Friends.getInstance().getPrefix() + DO_NOT_WRITE_THAT);
+	}
+
+	public boolean cancelled(SimpleMessageEvent pEvent) {
+		final OnlinePAFPlayer p = pEvent.getSender();
+		if (!p.hasPermission(PERMISSION)) {
+			String message = pEvent.getMessage().toLowerCase();
+			for (String forbiddenWord : FORBIDDEN)
+				if (message.contains(forbiddenWord)) {
+					p.sendMessage(DO_NOT_WRITE_THAT);
+					pEvent.setCancelled(true);
+					return true;
+				}
+		}
+		return false;
+	}
+}
